1a41f228e1907f478f1d414018678e7180a7648a
solomon amaechi <solomornejohn@gmail.com>
2026-01-19 11:38:57 +0100

    managing permissions and groupd in django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.conf import settings
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+    class Meta:
+        # Defining custom permissions
+        permissions = [
+            ("can_view", "Can view book details"),
+            ("can_create", "Can create a new book"),
+            ("can_edit", "Can edit book details"),
+            ("can_delete", "Can delete a book"),
+        ]
+
+    def __str__(self):
+        return self.title
 
 
 class CustomUserManager(BaseUserManager):
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 # Create your views here.
+# Enforcing permissions in views
+@permission_required('relationship_app.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
+
+@permission_required('relationship_app.can_create', raise_exception=True)
+def add_book(request):
+    # Logic to add a book
+    return render(request, 'relationship_app/add_book.html')
+
+@permission_required('relationship_app.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Logic to edit the book
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+@permission_required('relationship_app.can_delete', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Logic to delete the book
+    book.delete()
+    return redirect('book_list')
