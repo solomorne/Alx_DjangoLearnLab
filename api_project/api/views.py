86e238409046981af75836260af9ddd3a85aa44f
solomon amaechi <solomornejohn@gmail.com>
2026-01-30 11:32:34 +0100

    new django project with REST framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# New ViewSet for full CRUD
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
