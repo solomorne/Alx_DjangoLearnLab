7b830d808b36ce80d346e261f28624ee04ec33e6
solomon amaechi <solomornejohn@gmail.com>
2026-02-02 09:07:34 +0100

    Setting up a new Django project with custom serializers in Django REST framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Book
@@ -58,7 +58,7 @@ class BookDeleteView(generics.DestroyAPIView):
 
 # Enable filtering, search, and ordering
     filter_backends = [
-        DjangoFilterBackend,
+        rest_framework.DjangoFilterBackend,
         SearchFilter,
         OrderingFilter,
     ]
