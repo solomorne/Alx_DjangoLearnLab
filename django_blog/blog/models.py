3933012ab535d235b995ca88b743eacdc8201ad1
solomon amaechi <solomornejohn@gmail.com>
2026-02-12 15:53:23 +0100

    add advanced features: tagging and search function

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class Tag(models.Model):
@@ -15,7 +16,7 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
