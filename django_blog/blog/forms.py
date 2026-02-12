21a048d4fb184b28ab5ed0cb701aab38dc543e32
solomon amaechi <solomornejohn@gmail.com>
2026-02-12 15:56:04 +0100

    add advanced features: tagging and search function

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment, Tag
+from taggit.forms import TagWidget
 
 
 class RegisterForm(UserCreationForm):
@@ -18,10 +19,13 @@ class UpdateUserForm(forms.ModelForm):
         fields = ['username', 'email']
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(
-        required=False,
-        help_text="Enter tags separated by commas"
-    )
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),   # ✅ required
+        }
 
     class Meta:
         model = Post
